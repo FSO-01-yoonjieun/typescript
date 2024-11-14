1a513e5eb59497ead409255d08bcc7f40f6ad269
YOON JIEUN <yjeyje001020@naver.com>
2024-11-14 17:34:31 +0900

    commit

---
+++ my-typescript-project/src/index.js
@@ -0,0 +1,14 @@
+var myString = "Hello, TypeScript!";
+console.log(myString);
+var myNumber = 38;
+console.log(myNumber);
+var myBoolean = true;
+console.log(myBoolean);
+var myNull = null;
+console.log(myNull);
+var myAny = "I can be anything!";
+console.log(myAny);
+myAny = 25;
+console.log(myAny);
+myAny = true;
+console.log(myAny);
---
+++ my-typescript-project/src/index.ts
@@ -0,0 +1,25 @@
+
+let myString: string = "Hello, TypeScript!";
+console.log(myString);  
+
+
+let myNumber: number = 38;
+console.log(myNumber); 
+
+
+let myBoolean: boolean = true;
+console.log(myBoolean);  
+
+
+let myNull: null = null;
+console.log(myNull);
+
+
+let myAny: any = "I can be anything!";
+console.log(myAny); 
+
+myAny = 25;
+console.log(myAny); 
+
+myAny = true;
+console.log(myAny); 
